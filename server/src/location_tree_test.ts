8ea066018b740ccecb73e284fef6261304ea854a
Jaela Field <jaelaf@uw.edu>
2024-11-28 16:05:49 -0800

    hw9 starter

---
+++ server/src/location_tree_test.ts
@@ -0,0 +1,135 @@
+import * as assert from 'assert';
+import {
+    buildTree, findLocationsInRegion
+  } from './location_tree';
+
+
+describe('location_tree', function() {
+
+  it('buildTree', function() {
+    assert.deepStrictEqual(buildTree([]), {kind: "empty"});
+
+    assert.deepStrictEqual(buildTree([{x: 1, y: 1}]),
+        {kind: "single", loc: {x: 1, y: 1}});
+    assert.deepStrictEqual(buildTree([{x: 2, y: 2}]),
+        {kind: "single", loc: {x: 2, y: 2}});
+
+    assert.deepStrictEqual(buildTree([{x: 1, y: 1}, {x: 3, y: 3}]),
+        {kind: "split", at: {x: 2, y: 2},
+         nw: {kind: "single", loc: {x: 1, y: 1}},
+         ne: {kind: "empty"},
+         sw: {kind: "empty"},
+         se: {kind: "single", loc: {x: 3, y: 3}}});
+    assert.deepStrictEqual(buildTree([{x: 1, y: 3}, {x: 3, y: 1}]),
+        {kind: "split", at: {x: 2, y: 2},
+         nw: {kind: "empty"},
+         ne: {kind: "single", loc: {x: 3, y: 1}},
+         sw: {kind: "single", loc: {x: 1, y: 3}},
+         se: {kind: "empty"}});
+
+    assert.deepStrictEqual(buildTree(
+        [{x: 1, y: 1}, {x: 3, y: 3}, {x: 5, y: 5}, {x: 7, y: 7}]),
+        {kind: "split", at: {x: 4, y: 4},
+         nw: {kind: "split", at: {x: 2, y: 2},
+              nw: {kind: "single", loc: {x: 1, y: 1}},
+              ne: {kind: "empty"},
+              sw: {kind: "empty"},
+              se: {kind: "single", loc: {x: 3, y: 3}}},
+         ne: {kind: "empty"},
+         sw: {kind: "empty"},
+         se: {kind: "split", at: {x: 6, y: 6},
+              nw: {kind: "single", loc: {x: 5, y: 5}},
+              ne: {kind: "empty"},
+              sw: {kind: "empty"},
+              se: {kind: "single", loc: {x: 7, y: 7}}}});
+    assert.deepStrictEqual(buildTree(
+        [{x: 1, y: 1}, {x: 3, y: 3}, {x: 5, y: 3}, {x: 7, y: 1},
+         {x: 1, y: 7}, {x: 3, y: 5}, {x: 5, y: 5}, {x: 7, y: 7}]),
+        {kind: "split", at: {x: 4, y: 4},
+         nw: {kind: "split", at: {x: 2, y: 2},
+              nw: {kind: "single", loc: {x: 1, y: 1}},
+              ne: {kind: "empty"},
+              sw: {kind: "empty"},
+              se: {kind: "single", loc: {x: 3, y: 3}}},
+         ne: {kind: "split", at: {x: 6, y: 2},
+              nw: {kind: "empty"},
+              sw: {kind: "single", loc: {x: 5, y: 3}},
+              ne: {kind: "single", loc: {x: 7, y: 1}},
+              se: {kind: "empty"}},
+         sw: {kind: "split", at: {x: 2, y: 6},
+              nw: {kind: "empty"},
+              ne: {kind: "single", loc: {x: 3, y: 5}},
+              sw: {kind: "single", loc: {x: 1, y: 7}},
+              se: {kind: "empty"}},
+         se: {kind: "split", at: {x: 6, y: 6},
+              nw: {kind: "single", loc: {x: 5, y: 5}},
+              ne: {kind: "empty"},
+              sw: {kind: "empty"},
+              se: {kind: "single", loc: {x: 7, y: 7}}}});
+  });
+
+  it('findLocationsInRegion', function() {
+    assert.deepStrictEqual(findLocationsInRegion(
+        buildTree([]),
+        {x1: 1, x2: 2, y1: 1, y2: 2}),
+      []);
+
+    assert.deepStrictEqual(findLocationsInRegion(
+        buildTree([{x: 0, y: 0}]),
+        {x1: 1, x2: 3, y1: 1, y2: 3}),
+      []);
+    assert.deepStrictEqual(findLocationsInRegion(
+        buildTree([{x: 2, y: 2}]),
+        {x1: 1, x2: 3, y1: 1, y2: 3}),
+      [{x: 2, y: 2}]);
+
+    assert.deepStrictEqual(findLocationsInRegion(
+        buildTree([{x: 0, y: 0}, {x: 2, y: 2}]),
+        {x1: 1, x2: 3, y1: 1, y2: 3}),
+      [{x: 2, y: 2}]);
+    assert.deepStrictEqual(findLocationsInRegion(
+        buildTree([{x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2}, {x: 3, y: 3},
+                   {x: 4, y: 4}]),
+        {x1: 1, x2: 3, y1: 1, y2: 3}),
+      [{x: 1, y: 1}, {x: 2, y: 2}, {x: 3, y: 3}]);
+    assert.deepStrictEqual(findLocationsInRegion(
+        buildTree([{x: 0, y: 4}, {x: 1, y: 3}, {x: 2, y: 2}, {x: 3, y: 4},
+                   {x: 4, y: 0}]),
+        {x1: 1, x2: 3, y1: 1, y2: 3}),
+      [{x: 2, y: 2}, {x: 1, y: 3}]);
+  });
+
+  it('closestInTree', function() {
+    // TODO: implement this in Task 4
+  });
+
+/* TODO: uncomment these in Task 4
+  it('findClosestInTree', function() {
+    assert.deepStrictEqual(findClosestInTree(
+        buildTree([{x: 2, y: 1}]),
+        [{x: 1, y: 1}]),
+      [{x: 2, y: 1}, 1]);
+    assert.deepStrictEqual(findClosestInTree(
+        buildTree([{x: 3, y: 1}, {x: 2, y: 1}, {x: 1, y: 3}]),
+        [{x: 1, y: 1}]),
+      [{x: 2, y: 1}, 1]);
+    assert.deepStrictEqual(findClosestInTree(
+        buildTree([{x: 1, y: 1}, {x: 1, y: 5}, {x: 5, y: 1}, {x: 5, y: 5}]),
+        [{x: 2, y: 1}]),
+      [{x: 1, y: 1}, 1]);
+    assert.deepStrictEqual(findClosestInTree(
+        buildTree([{x: 1, y: 1}, {x: 1, y: 5}, {x: 5, y: 1}, {x: 5, y: 5}]),
+        [{x: 2, y: 1}, {x: 4.9, y: 4.9}]),
+      [{x: 5, y: 5}, Math.sqrt((5-4.9)**2+(5-4.9)**2)]);
+    assert.deepStrictEqual(findClosestInTree(
+        buildTree([{x: 1, y: 1}, {x: 1, y: 5}, {x: 5, y: 1}, {x: 5, y: 5}]),
+        [{x: 2, y: 1}, {x: -1, y: -1}]),
+      [{x: 1, y: 1}, 1]);
+    assert.deepStrictEqual(findClosestInTree(
+        buildTree([{x: 1, y: 1}, {x: 1, y: 5}, {x: 5, y: 1}, {x: 5, y: 5}]),
+        [{x: 4, y: 1}, {x: -1, y: -1}, {x: 10, y: 10}]),
+      [{x: 5, y: 1}, 1]);
+  });
+*/
+
+});
